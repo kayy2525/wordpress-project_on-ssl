20e3063f4d66cf09626ec7022216e99005578c28
Vyaheslav Platon <vyacheslav25sl@gmail.com>
2021-03-02 16:27:15 +0200

    add commentary to script backup.sh

--- backup_full.sh
+++ backup_full.sh
@@ -2,3 +2,4 @@ docker exec -it db_for_wordpress mysqldump -p123qweASDZXC -u root wordpress_db >
 cd /srv/projects/dockers/wordpress-project/ && docker-compose down && sudo git add . && sudo git commit -m "backup_full" -u && sudo git push -f origin master
 tar -czvf backup_full.tar.gz /srv/projects/dockers/wordpress-project && cp /srv/projects/dockers/wordpress-project/backup_full.tar.gz /mnt/storage02/backups/project_wordpress_mazur/
 cd /srv/projects/dockers/wordpress-project/ && docker-compose up -d
+echo "FINISH BACKUP"
